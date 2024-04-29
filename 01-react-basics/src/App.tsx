a8f032bfb36d6399c405288a5769d5d59c0e21f8
MohammadMzien2 <mohammadlu281@gmail.com>
2024-04-29 11:43:28 +0200

    stateful variabel

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function App() {
 	const [counter, setCounter] = useState(1);
+	const [msg, setMsg] = useState("Hi");
 	//let counter = 1;
 
 	console.log("App is being redered")
@@ -17,9 +18,15 @@ function App() {
 		<div className='container'>
 			<h1>01-react-basic</h1>
 
+			<p>{msg}</p>
+
 			<p>Counter: {counter}</p>
 
+			
+
 			<button onClick={handleBtnClick} className="btn btn-success">Click me</button>
+			<button onClick={() => setMsg("HI sun")} className="btn btn-warning"> HI </button>
+
 		</div >
 	);
 }
