d0f0bdfa11da4fde0d6b4a0b5e7dec23828539ae
MohammadMzien2 <mohammadlu281@gmail.com>
2024-05-06 12:01:47 +0200

    Fetch data from API on resource state change.

--- 04-fetch/src/App.tsx
+++ 04-fetch/src/App.tsx
@@ -3,18 +3,39 @@ import { Resource } from "./types/Resource";
 import "./assets/App.scss";
 
 function App() {
-	const [resource, setResource] = useState('posts');
+	const [resource, setResource] = useState("");
 	const [data, setData] = useState<Resource[]>([]);
+
+	// Hämta api
+	useEffect(() => {
+		if (!resource) {
+			return;
+		}
+
+		console.log("Side-effect triggered due to resource changing value", resource);
+
+		const fetchData = async () => {
+			const res = await fetch(`https://jsonplaceholder.typicode.com/${resource}`);
+			const payload = await res.json();
+			setData(payload);
+		}
+		fetchData();
+	}, [resource]);
+
+	console.log("Rendering... Resource is:", resource);
+
 
 	return (
 		<div className="container">
-			<h1 className="mb-3">Fetch</h1>
+			<h1 className="mb-3">🐶 Fetch</h1>
 
-			<div className="d-flex justify-content-between">
+			<div className="d-flex justify-content-between m-5">
 				<button onClick={() => setResource('albums')} className="btn btn-primary">Albums</button>
 				<button onClick={() => setResource('photos')} className="btn btn-success">Photos</button>
 				<button onClick={() => setResource('posts')} className="btn btn-warning">Posts</button>
 				<button onClick={() => setResource('todos')} className="btn btn-danger">Todos</button>
+				<button onClick={() => setResource('memes')} className="btn btn-info">Memes</button>
+
 			</div>
 
 			{data && (
@@ -24,7 +45,7 @@ function App() {
 
 					<ol>
 						{data.map(item => (
-							<li>{item.title}</li>
+							<li key={item.id}>{item.title}</li>
 						))}
 					</ol>
 				</>
