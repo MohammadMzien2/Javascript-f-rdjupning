442ae7a316cd0961e3f1fda3f5e251969bb66b55
Your Name <you@example.com>
2024-05-23 09:20:55 +0200

    Created more generic data-fetching hook useGetData that receive the type of data (`T`) and an optional `initialUrl`. Replaced all logic in `useGetRandomDogImage` with a call to `useGetData`.

--- 07-hackernews/src/components/Navigation.tsx
+++ 07-hackernews/src/components/Navigation.tsx
@@ -1,9 +1,13 @@
+import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { Link, NavLink } from "react-router-dom";
+import useTheme from "../hooks/useTheme";
 
 const Navigation = () => {
+	const { isDarkMode, toggleTheme } = useTheme();
+
 	return (
 		<Navbar bg="dark" variant="dark" expand="md">
 			<Container>
@@ -12,9 +16,14 @@ const Navigation = () => {
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
-						<Nav.Link as={NavLink} end to="/search">Search</Nav.Link>
+						<Nav.Link as={NavLink} end to="/random-dog">🐶 Random dog</Nav.Link>
+						<Nav.Link as={NavLink} end to="/search">🔎 Search</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
+
+				<Button variant="outline-secondary" onClick={toggleTheme}>
+					{isDarkMode ? "☀️" : "🌙"}
+				</Button>
 			</Container>
 		</Navbar>
 	)
